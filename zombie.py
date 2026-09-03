fa9a3447199dda9b4d6640c08d1fe426efaa78ea
Exy Sabbe <exy.sabbe@sintlievenscollege.be>
2026-09-03 14:12:47 +0200

    Added basic wander behavior to zombies

--- zombie.py
+++ zombie.py
@@ -1,10 +1,18 @@
 import pygame
+import random
+import math
 from character import *
 
 class Zombie(Character):
+    def __init__(self, width, height, x_pos, y_pos, speed, health, map, image_path, corr_angle):
+        Character.__init__(self, width, height, x_pos, y_pos, speed, health, map, image_path, corr_angle,)
+        self.__angle = random.randint(1, 360)
+
     def Zombiebehavior(self):
-        self.SetMoveDirection((0, -1))
-        self.LookAt((self.GetCenterPos()[0] + 0, self.GetCenterPos()[1] - 1))
+        dir = (math.cos(math.radians(self.__angle)), math.sin(math.radians(self.__angle)))
+        self.__angle += random.randint(-10, 10) / 10
+        self.SetMoveDirection(dir)
+        self.LookAt((self.GetCenterPos()[0] + dir[0], self.GetCenterPos()[1] - dir[1]))
 
     def Update(self, delta_time):
         self.Zombiebehavior()
